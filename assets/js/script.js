e9f0b75b4292ba6606da59cc3199aecf84a46c8f
mukukuu <qinii@me.com>
2021-12-01 19:53:43 +0100

    created fuction to add elements when textarea and dateinput are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,70 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+//task text was clicked
+$(".list-group").on("click", "p", function() {
+  //get current text of p element
+  var text = $(this)
+  .text()
+  .trim();
+
+  //replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  //auto focus new element
+  textInput.trigger("focus");
+});
+
+//editable field was un-focused 
+$(".list-group").on("blur", "textarea", function() {
+  //get current value of textarea
+  var text = $(this).val();
+
+  //get status type and position in the list
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  //update task in array and re-save to localstore
+  task[status][index].text = text;
+  saveTasks();
+
+  //recreate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  //replace textarea with new content
+  $(this).replaceWith(taskP);
+});
+
+//due date was clicked 
+$(".list-group").on("click", "span", function() {
+  //get current text
+  var date = $(this)
+  .text()
+  .trim();
+
+  //create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+  $(this).replaceWith(dateInput);
+
+  //automatically bring up the calender
+  dateInput.trigger("focus");
+});
+
+
+
+
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -89,6 +153,9 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+  $(".list-group").on("click", "p", function() {
+    console.log("<p> was clicked");
+  });
 });
 
 // load tasks for the first time
