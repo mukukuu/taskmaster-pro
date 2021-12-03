a6b1cd6c2838477e37e695cbd8865dabaf1a4d01
mukukuu <qinii@me.com>
2021-12-03 10:18:50 -0500

    added time audit for near/over due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,28 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl)
+  .find("span")
+  .text()
+  .trim();
 
+  //convert to momemt object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  //remove any old classes from element 
+  $(taskEl),removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+//enable dragable/sortable feature on list-group elements
 
 
 // modal was triggered
